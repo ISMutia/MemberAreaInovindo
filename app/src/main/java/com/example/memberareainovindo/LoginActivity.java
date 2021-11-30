a57bd29c35f7e6912164c5869413c61263696841
Mutia <adikmutia@gmail.com>
2021-11-30 20:26:24 +0700

    comiit new

--- app/src/main/java/com/example/memberareainovindo/LoginActivity.java
+++ app/src/main/java/com/example/memberareainovindo/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -12,6 +13,8 @@ public class LoginActivity extends AppCompatActivity {
 
     TextView textRegister;
     TextView textLupaPassword;
+    EditText email, password;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,9 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         textRegister = (TextView) findViewById(R.id.signup);
         textLupaPassword = (TextView) findViewById(R.id.lupa_password);
+        email = (EditText) findViewById(R.id.m_email);
+        password = (EditText) findViewById(R.id.m_password);
+        button = (Button) findViewById(R.id.btnLogin);
         textLupaPassword();
         textRegister();
 
