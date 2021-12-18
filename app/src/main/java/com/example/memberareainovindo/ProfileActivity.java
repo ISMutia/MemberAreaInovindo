6c83f3e8d2a8f3b9f0eb1a2677080295c90dc516
Mutia <adikmutia@gmail.com>
2021-12-18 15:40:18 +0700

    Commit New

--- app/src/main/java/com/example/memberareainovindo/ProfileActivity.java
+++ app/src/main/java/com/example/memberareainovindo/ProfileActivity.java
@@ -1,14 +1,71 @@
 package com.example.memberareainovindo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.memberareainovindo.data.SessionManager;
+import com.example.memberareainovindo.databinding.ActivityAddOrderBinding;
+import com.example.memberareainovindo.databinding.ActivityProfileBinding;
+import com.example.memberareainovindo.ui.dashboard.MainActivity;
+import com.example.memberareainovindo.ui.login.LoginActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private ActivityProfileBinding binding;
+    private SessionManager mSessionManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        binding = ActivityProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mSessionManager = new SessionManager(this);
+
+        initOnClick();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.buttom_navigation);
+
+        bottomNavigationView.setSelectedItemId(R.id.profile);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.notification:
+                        Intent gotonotif = new Intent(ProfileActivity.this, NotificationActivity.class);
+                        startActivity(gotonotif);
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.menu1:
+                        Intent gotohome = new Intent(ProfileActivity.this, MainActivity.class);
+                        startActivity(gotohome);
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.profile:
+                        return true;
+                }
+                return false;
+            }
+        });
     }
+
+    private void initOnClick() {
+        binding.btnOut.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                mSessionManager.logout();
+                Intent gotologin = new Intent(ProfileActivity.this, LoginActivity.class);
+                startActivity(gotologin);
+                return false;
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/memberareainovindo/data/SessionManager.java
+++ app/src/main/java/com/example/memberareainovindo/data/SessionManager.java
@@ -6,6 +6,14 @@ import android.content.SharedPreferences;
 public class SessionManager {
     private String PREF_NAME = "SIMPAN";
     private String KEY_FULLNAME = "fullname";
+    private String KEY_DATE_BIRTH = "date_birth";
+    private String KEY_EMAIL = "email";
+    private String KEY_GAMBAR = "gambar";
+    private String KEY_STATUS = "status";
+    private String KEY_CONTACT_WA = "contact_wa";
+    private String KEY_ADDRESS = "address";
+    private String KEY_CREATED_AT = "created_at";
+    private String KEY_UPDATED_AT = "updated_at";
     private String KEY_ID_USER = "id";
 
 
@@ -23,6 +31,69 @@ public class SessionManager {
 
     public String getFullname() {
         return mSharedPreferences.getString(KEY_FULLNAME, null);
+    }
+
+    public void setDatebirth(String date_birth) {
+        mEditor = mSharedPreferences.edit();
+        mEditor.putString(KEY_DATE_BIRTH, date_birth).apply();
+    }
+
+    public String getDateBirth() {
+        return mSharedPreferences.getString(KEY_DATE_BIRTH, null);
+    }
+
+    public void setEmail(String email) {
+        mEditor = mSharedPreferences.edit();
+        mEditor.putString(KEY_EMAIL, email).apply();
+    }
+
+    public String getEmail() {
+        return mSharedPreferences.getString(KEY_EMAIL, null);
+    }
+
+    public void setGambar(String gambar) {
+        mEditor = mSharedPreferences.edit();
+        mEditor.putString(KEY_GAMBAR, gambar).apply();
+    }
+
+    public String getGambar() {
+        return mSharedPreferences.getString(KEY_GAMBAR, null);
+    }
+
+    public void setStatus(String status) {
+        mEditor = mSharedPreferences.edit();
+        mEditor.putString(KEY_STATUS, status).apply();
+    }
+
+    public String getStatus() {
+        return mSharedPreferences.getString(KEY_STATUS, null);
+    }
+
+    public void setContactWa(String contact_wa) {
+        mEditor = mSharedPreferences.edit();
+        mEditor.putString(KEY_CONTACT_WA, contact_wa).apply();
+    }
+
+    public String getContactWa() {
+        return mSharedPreferences.getString(KEY_CONTACT_WA, null);
+    }
+
+    public void setAddress(String address) {
+        mEditor = mSharedPreferences.edit();
+        mEditor.putString(KEY_ADDRESS, address).apply();
+    }
+
+    public String getAddress() {
+        return mSharedPreferences.getString(KEY_ADDRESS, null);
+    }
+
+    public void setCreatedAt(String created_at) {
+        mEditor = mSharedPreferences.edit();
+        mEditor.putString(KEY_CREATED_AT, created_at).apply();
+    }
+
+    public String getUpdateAt() {
+        return mSharedPreferences.getString(KEY_UPDATED_AT, null);
     }
 
     public void logout() {
